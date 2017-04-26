eb403493439388a2af5e2367962f4eb3395f9a26
David Alsh <alshdavid@gmail.com>
2017-04-26 20:24:12 +1200

    ok

--- other.js
+++ other.js
@@ -29,11 +29,11 @@ function update(application, request){
          let ls = spawn('sh', ['deploy.sh'], { cwd : application.cwd});
 
          ls.stdout.on('data', (data) => {
-         //  console.log(`stdout: ${data}`);
+            console.log(`stdout: ${data}`);
          });
 
          ls.stderr.on('data', (data) => {
-         //  console.log(`stderr: ${data}`);
+             console.log(`stderr: ${data}`);
          });
 
          ls.on('close', (code) => {
