3e0aad07fde64901ab04174345b08115991c2645
David Alsh <alshdavid@gmail.com>
2017-04-26 17:19:43 +1200

    yep

---
+++ index.js
@@ -0,0 +1,94 @@
+// Clear CMD
+process.stdout.write('\u001b[2J\u001b[0;0H');
+
+// Imports
+const fs = require("fs")
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const exec = require('child_process').exec;
+const spawn = require('child_process').spawn;
+const request = require('request');
+
+// Init
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Load Config
+const config = JSON.parse( fs.readFileSync('./deploy.json', 'utf8') )
+
+// Add To Config
+config.deployments.forEach(project => project['locked'] = false)
+config.deployments.forEach(project => project['retry']  = false)
+
+
+app.get('/deploy/:route', (req, res) => {
+    let project = config.deployments.find(x => x.route == req.params.route)
+
+    if (!project) { res.status(404); return res.send("404") } 
+    res.send(project.name)
+
+    if (project.type == "github")     if ( !gitHub(project.branch, req) ) return
+    if (project.type == "bitbucket")  if ( !bitBucket(project.branch, req) ) return
+    
+
+    console.log("test")
+    //update(project)
+
+
+})
+
+app.listen(config.settings.port, function () {
+  console.log('Deployer listening on port ' + config.settings.port)
+})
+
+
+function bitBucket(branch, request){
+    let req_branch
+
+    try {
+        req_branch = request.push.changes[0].new.name
+    }
+    catch (err) {
+        console.log('Invalid Request')
+        rej("Invalid Request")
+        return false
+    }
+    
+    return true
+}
+
+function gitHub(branch, request){
+    return true
+}
+
+// function update(application){
+//    return new Promise ((res, rej)=>{
+
+//       if(req_branch == application.branch && !application.locked){
+//          application.locked = true 
+//          console.log("Build Started     @ sh " + application.cwd + "deploy.sh")
+
+//          let ls = spawn('sh', ['deploy.sh'], { cwd : application.cwd});
+
+//          ls.stdout.on('data', (data) => {
+//          //  console.log(`stdout: ${data}`);
+//          });
+
+//          ls.stderr.on('data', (data) => {
+//          //  console.log(`stderr: ${data}`);
+//          });
+
+//          ls.on('close', (code) => {
+//              console.log("Build Completed   @ sh " + application.cwd + "deploy.sh")
+//              application.locked = false
+//              slack_it("Build Completed", application.hook)
+//              res("Completed")
+//          }); 
+
+//       } else {
+//          console.log('Not Master')
+//          rej("Not Master")
+//       }
+//    })
+// }
---
+++ other.js
@@ -0,0 +1,88 @@
+
+
+function slack_it(message, url){
+    request({
+       url: url,
+       method : 'POST',
+       json : true,
+       body : { "text" : message }
+   })
+}
+
+function update(application, request){
+   return new Promise ((res, rej)=>{
+      let req_branch
+
+      try {
+         req_branch = request.push.changes[0].new.name
+      }
+      catch (err) {
+         console.log('Invalid Request')
+         rej("Invalid Request")
+         return
+      }
+
+      if(req_branch == application.branch && !application.locked){
+         application.locked = true 
+         console.log("Build Started     @ sh " + application.cwd + "deploy.sh")
+
+         let ls = spawn('sh', ['deploy.sh'], { cwd : application.cwd});
+
+         ls.stdout.on('data', (data) => {
+         //  console.log(`stdout: ${data}`);
+         });
+
+         ls.stderr.on('data', (data) => {
+         //  console.log(`stderr: ${data}`);
+         });
+
+         ls.on('close', (code) => {
+             console.log("Build Completed   @ sh " + application.cwd + "deploy.sh")
+             application.locked = false
+             slack_it("Build Completed", application.hook)
+             res("Completed")
+         }); 
+
+      } else {
+         console.log('Not Master')
+         rej("Not Master")
+      }
+   })
+}
+
+app.post(hooks.api.route, (req, res) => {
+   update(hooks.api, req.body)
+   res.send("Thanks")
+})
+
+app.post(hooks.app.route, (req, res) => {
+   update(hooks.app, req.body)
+   res.send("Thanks")
+})
+
+
+var config = {
+    "credentials" : {
+        "username" : "digitalfoundry",
+        "password" : "lama lama red migo"
+    },
+    "settings" : {
+        "port" : "4001"
+    },
+    "deployments" : [
+        {
+            "name" : "Marshall Server",
+            ""
+        }
+    ]
+}
+
+
+var app = {
+       hook : "https://hooks.slack.com/services/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",
+       route : '/65b22229-eee8-4f8a-b867-d34392d4fb79',
+       command : 'sudo sh /var/www/html/admin/deploy.sh',
+       cwd : "/var/www/html/admin/",
+       branch : 'master',
+       locked : false
+   }
